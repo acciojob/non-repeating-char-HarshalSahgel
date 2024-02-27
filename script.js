7b5f4b0b6c2bd2010b24100be18380dd5276ca20
Harshal Sahgel <129950309+HarshalSahgel@users.noreply.github.com>
2024-02-27 19:56:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+
+	const charNum = new Map();
+
+	for(let char of str){
+		charNum.set(char, (charNum.get(char) || 0) + 1);
+	}
+	for(const char of str){
+		if(charNum.get(char) === 1){
+			return char;
+		}
+	}
+
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
